1751a43ce9897b23a8673ae00ebd48f5898ccf75
FethyAmr <74984977+FethyAmr@users.noreply.github.com>
2021-04-30 17:36:01 +0200

    back

---
+++ api-countries-react/dataCountries.js
@@ -0,0 +1,24 @@
+const countries = [
+    {
+        name: "France",
+        flag: 'https://restcountries.eu/data/fra.svg',
+        capital: "Paris",
+        region: "Europe",
+        population: 66710000
+    },
+    {
+        name: "Brazil",
+        flag: 'https://restcountries.eu/data/bra.svg',
+        capital: "Brasília",
+        region: "Americas",
+        population: 206135893
+    },
+    {
+        name: "Croatia",
+        flag: 'https://restcountries.eu/data/bra.svg',
+        capital: "Zagreb",
+        region: "Europe",
+        population: 4190669
+    }
+]
+module.exports = countries
