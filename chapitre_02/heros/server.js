bee3b6d83f80b40137b2820fc297923f74992c03
FethyAmr <74984977+FethyAmr@users.noreply.github.com>
2021-05-27 16:01:43 +0200

    heros

---
+++ chapitre_02/heros/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+const app = express();
+import cors from 'cors';
+
+app.use(express.json())
+app.use(cors())
+
+const debug = (req, res, next) => {
+    console.log("Server request")
+    next()
+}
+
+const port = 8000;
+app.listen(port, () => console.log('Server', port))
