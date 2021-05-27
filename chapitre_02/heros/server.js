4571ada47a08797b1a2c6baecc654835741bbda1
FethyAmr <74984977+FethyAmr@users.noreply.github.com>
2021-05-27 17:23:18 +0200

    heros²

--- chapitre_02/heros/server.js
+++ chapitre_02/heros/server.js
@@ -12,3 +12,31 @@ const debug = (req, res, next) => {
 
 const port = 8000;
 app.listen(port, () => console.log('Server', port))
+
+
+const superHeros = [
+    {
+        name: "Iron Man",
+        power: ["money"],
+        color: "red",
+        isAlive: true,
+        age: 46,
+        image: "https://blog.fr.playstation.com/tachyon/sites/10/2019/07/unnamed-file-18.jpg?resize=1088,500&crop_strategy=smart"
+    },
+    {
+        name: "Thor",
+        power: ["electricty", "worthy"],
+        color: "blue",
+        isAlive: true,
+        age: 300,
+        image: "https://www.bdfugue.com/media/catalog/product/cache/1/image/400x/17f82f742ffe127f42dca9de82fb58b1/9/7/9782809465761_1_75.jpg"
+    },
+    {
+        name: "Daredevil",
+        power: ["blind"],
+        color: "red",
+        isAlive: false,
+        age: 30,
+        image: "https://aws.vdkimg.com/film/2/5/1/1/251170_backdrop_scale_1280xauto.jpg"
+    }
+]
