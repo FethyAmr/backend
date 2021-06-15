8639741ad825993ccf39a7cdc19a9bf37302b422
FethyAmr <74984977+FethyAmr@users.noreply.github.com>
2021-06-15 17:32:35 +0200

    Debut

--- chapitre_04/Validation.js
+++ chapitre_04/Validation.js
@@ -1,12 +1,41 @@
 const express = require('express');
 const expressValidator = require("express-validator");
 const passwordValidator = require('password-validator');
+
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
 
 mongoose.connect("mongodb://localhost:27017/validator");
 
+
+app.get("/", (req, res) => {
+
+    res.send()
+
+})
+
+
+
+app.post("/users/add", (req, res) => {
+
+    res.send()
+
+})
+
+
+app.get("/users/:username", (req,res) => {
+
+    res.send()
+
+})
+
+
+app.get("/users/:email", (req, res) => {
+
+    res.send()
+
+})
 
 
 app.use(express.static('public'));
