90440156249854f59d2dd2dfa6849a9cb91c0ae5
FethyAmr <74984977+FethyAmr@users.noreply.github.com>
2021-06-17 16:34:24 +0200

    validatiion

--- chapitre_04/controllers/controllersUsers.js
+++ chapitre_04/controllers/controllersUsers.js
@@ -8,17 +8,17 @@ const addUser = async (req, res) => {
         const profilUser = req.body
 
         if (!errors.isEmpty()) {
-            res.status(400).json({message : "There is a problem with your form please correct this"});
+            res.status(400).json({ message: "There is a problem, please correct this" });
         } else {
-            
-            const cityFound = await City.findOne({name : profilUser.city})
+
+            const cityFound = await City.findOne({ name: profilUser.city })
             profilUser.city = cityFound
 
             await User.create(profilUser)
-            res.json({message : 'User added'})
+            res.json({ message: 'User added' })
         }
     } catch (error) {
-        res.status(500).json({ errorMessage: "There is a problem !!!" })
+        res.status(500).json({ errorMessage: "There is a problem !!" })
     }
 }
 
@@ -26,24 +26,57 @@ const sendUserByUsername = async (req, res) => {
     try {
         const usernameReceived = req.params.username;
 
-        const userFound = await User.findOne({username : usernameReceived }).populate('city', 'name -_id')
+        const userFound = await User.findOne({ username: usernameReceived }).populate('city', 'name -_id')
 
         res.json(userFound)
 
     } catch (error) {
-        res.status(500).json({ errorMessage: "There is a problem !!!" })
+        res.status(500).json({ errorMessage: "There is a problem !!" })
     }
 }
 
 const sendUserByEmail = async (req, res) => {
     try {
-        res.json({message : 'yes !!'})
+        res.json({ message: 'yes !!' })
     } catch (error) {
-        res.status(500).json({ errorMessage: "There is a problem !!!" })
+        res.status(500).json({ errorMessage: "There is a problem !!" })
     }
 
 }
 
+const sendUserByEmail = async (req, res, next) => {
+    try {
+        const value = req.params.value
+        const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (!(regex.test(value))) {
+            next()
+        } else {
+
+            const findEmail = await User.findOne({ email: value }).populate('city', 'name -_id')
+
+            if (!findEmail) {
+                res.json({ message: "invalid email" })
+            }
+
+            res.json(findEmail)
+        }
+    } catch (error) {
+        res.status(500).json({ errorMessage: "There is a problem !!" })
+    }
+}
+
+const sendUserById = async (req, res, next) => {
+    try {
+        const value = req.params.valu
+
+        if
 
 
-module.exports = { addUser, sendUserByUsername, sendUserByEmail}
+
+
+
+
+
+
+
+module.exports = { addUser, sendUserByUsername, sendUserByEmail }
--- chapitre_04/model/routes.js
+++ chapitre_04/model/routes.js
@@ -1,10 +1,8 @@
-router.get("/add",
+const expressValidator = require("express-validator");
+
+app.post("/user/add",
     expressValidator.body("username").isLength({ min: 4 }),
     expressValidator.body("email").isEmail(),
     expressValidator.body("age").isInt().isLength(({ min: 2, max: 2 })),
     expressValidator.body("city").equals("Paris" || "Tokyo" || "Los Angeles")
-)
-app.listen(8000, () => {
-    console.log('Server started');
-});
-
+) 
